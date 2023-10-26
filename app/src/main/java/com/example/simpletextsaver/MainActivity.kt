a791ee3255d80f4a83dfa01c7c0a7f563bc4198e
Zeinab <sz.marashi87@gmail.com>
2023-10-26 10:14:49 -0700

    AppBar added

--- app/src/main/java/com/example/simpletextsaver/MainActivity.kt
+++ app/src/main/java/com/example/simpletextsaver/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.simpletextsaver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.simpletextsaver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(binding.toolBarMain)
     }
 }

7a926f706421848e1ff74a73459391f7d1f5f30d
Zeinab <sz.marashi87@gmail.com>
2023-11-28 10:51:43 -0800

    sharedPreferences added

--- app/src/main/java/com/example/simpletextsaver/Profile.kt
+++ app/src/main/java/com/example/simpletextsaver/Profile.kt
@@ -1,28 +1,38 @@
+
+
 package com.example.simpletextsaver
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 import com.example.simpletextsaver.databinding.FragmentProfileBinding
 
 class Profile : Fragment() {
     lateinit var binding: FragmentProfileBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentProfileBinding.inflate(layoutInflater)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val navController = findNavController()
-        val inclusive = true
-        navController.popBackStack(R.id.profile, inclusive)
+
+        val sharedPreferences =
+            requireActivity().getPreferences(Context.MODE_PRIVATE)
+        val userName = sharedPreferences.getString("USERNAME_KEY", "")
+        val email = sharedPreferences.getString("EMAIL_KEY", "")
+        val password = sharedPreferences.getString("PASSWORD_KEY", "")
+
+        binding.txtUsernameProfile.text = userName
+        binding.txtEmailProfile.text = email
+        binding.txtPasswordProfile.text = password
     }
-    }
+}
--- app/src/main/java/com/example/simpletextsaver/Rigester.kt
+++ app/src/main/java/com/example/simpletextsaver/Rigester.kt
@@ -1,29 +1,62 @@
+
 package com.example.simpletextsaver
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.simpletextsaver.databinding.FragmentRigesterBinding
 
-
 class Rigester : Fragment() {
-    lateinit var binding:FragmentRigesterBinding
+    lateinit var binding: FragmentRigesterBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentRigesterBinding.inflate(layoutInflater)
-        return binding.root    }
+        binding = FragmentRigesterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val navController = findNavController()
-        val inclusive = true
-        navController.popBackStack(R.id.rigester, inclusive)
+        binding.btnSubmit.setOnClickListener {
+            if (binding.txtEmailRigester.length() > 0 &&
+                binding.txtUsernameRigester.length() > 0 &&
+                binding.txtPasswordRigester.length() > 0
+            ) {
+                saveUserData(
+                    binding.txtUsernameRigester.text.toString(),
+                    binding.txtEmailRigester.text.toString(),
+                    binding.txtPasswordRigester.text.toString()
+                )
+
+                val action =
+                    RigesterDirections.actionRigesterToProfile(
+                        binding.txtUsernameRigester.text.toString(),
+                        binding.txtEmailRigester.text.toString(),
+                        binding.txtPasswordRigester.text.toString()
+                    )
+                findNavController().navigate(action)
+
+                (requireActivity() as MainActivity).setBottomNavigationSelectedItem(R.id.profile)
+            }
         }
     }
+
+    private fun saveUserData(username: String, email: String, password: String) {
+        val sharedPreferences =
+            requireActivity().getPreferences(Context.MODE_PRIVATE)
+        with(sharedPreferences.edit()) {
+            putString("USERNAME_KEY", username)
+            putString("EMAIL_KEY", email)
+            putString("PASSWORD_KEY", password)
+            apply()
+        }
+    }
+}
